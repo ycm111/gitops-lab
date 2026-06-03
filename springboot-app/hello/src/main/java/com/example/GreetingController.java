fc20327275c735419b1d9cb906e84912b33914c2
ycm111 <ycm6666@gmail.com>
2026-06-03 17:36:56 +0900

    내용수정3

--- springboot-app/hello/src/main/java/com/example/GreetingController.java
+++ springboot-app/hello/src/main/java/com/example/GreetingController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 
-	private static final String template = "Hello, %s ㅎㅎㅎ\n";
+	private static final String template = "Hello, %s ^oo^";
 	private final AtomicLong counter = new AtomicLong();
 
 	@GetMapping("/hello")
